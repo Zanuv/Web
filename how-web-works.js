b7bb97752bd6292a26fc7bce29dc184307c81cfe
Zach <zanuv@outlook.com>
2022-11-20 15:32:24 -0600

    first commit

---
+++ how-web-works.js
@@ -0,0 +1,31 @@
+// What is HTTP?
+// HTTP stands for Hyper Text Transfer Protocol, it governs how clients get data from, or send data to, a server.
+
+// What is a URL?
+// URL stands for Uniform Resource Locator, a URL is an address for some internet resource.
+
+// What is DNS?
+// DNS stands for Domain Name System, this is a system that takes human-readable URLs and converts them into IP addresses.
+
+// What is a query string?
+// The query string allows you to pass key-value pairs into the URL
+
+// What is an HTTP request?
+// An HTTP request is a request from a client to a server which follows the HTTP protocol
+// A request for HTML
+
+// What is an HTTP response?
+// An HTTP response is a response from a server to a client which follows the HTTP protocol
+// Sends HTML, CSS, JS
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// Headers provide additional information about the request or the response
+// Request Headers - Host, User-Agent, Accept, Cookie, Cache-control
+// Response Headers - Content-type, Last-Modified, Set-Cookie, Cache-control
+
+// What happens when you type a URL in a browser?
+// 1. The browser resolves the name into an IP address using DNS
+// 2. The browser makes a request to that IP address, including headers
+// 3. The server sends a response (HTML with a status code)
+// 4. The browser makes a DOM from the returned HTML and finds any other resources needed
+// 5. The browser makes a separate HTTP request for those resources and receives responses from the server for each
